4dc4004380951af6dac1400cb1454161381011db
vishnu.vardhaninfyshine <vishnu.vardhan@infyshine.com>
2023-06-02 14:42:12 +0530

    Second Commit

---
+++ src/main/java/com/xinthe/pms/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.xinthe.pms.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        logger.error("An exception occurred: {}", ex.getMessage());
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/com/xinthe/pms/model/PmsCollection.java
@@ -0,0 +1,47 @@
+package com.xinthe.pms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PmsCollection implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+	private String fProjectName;
+	private String fCode;
+	private String fDescription;
+	private Date fStartDate;
+	private Date fEndDate;
+	private String fDomain;
+	private String fProgram;
+	private String fPortfolio;
+	private String fBudget;
+	private String fStatus;
+	private String fPriority;
+	private String fOwnerName;
+	private String fOwnerEmail;
+	private String fMethodology;
+	private String fCountry;
+	private String fState;
+	private Date createdDate;
+	private Date updatedDate;
+	private String createdBy;
+	private String updatedBy;
+	private List<Screen> screens;
+	private boolean showOnWeb;
+	private boolean showOnMobile;
+}
+
